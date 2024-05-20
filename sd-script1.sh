b6651c608a02a06f57999b324d5fbbf4ce35c45f
Selva <47226341+selvadhurai@users.noreply.github.com>
2024-05-20 11:18:13 -0400

    Update sd-script1.sh

--- sd-script1.sh
+++ sd-script1.sh
@@ -5,10 +5,11 @@ DB_HOST="sd-db1.c76o4qoeq8xw.us-east-1.rds.amazonaws.com"
 DB_NAME="sd-db1"
 DB_USER="sd-db1"
 DB_PASS="test1234"
+# SQL script file
 SQL_FILE="release1.sql"
 
 # Connect to the database
-psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER" -w -c "SELECT 1" > /dev/null 2>&1
+psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
 
 # Check if authentication was successful
 if [ $? -eq 0 ]; then
