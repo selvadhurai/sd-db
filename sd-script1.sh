1f75706d17167d750fd1984b6991d8ad363e4968
Selva <47226341+selvadhurai@users.noreply.github.com>
2024-05-21 16:40:04 -0400

    Update sd-script1.sh

--- sd-script1.sh
+++ sd-script1.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Database connection parameters
 DB_HOST="sd-db1.c76o4qoeq8xw.us-east-1.rds.amazonaws.com"
 DB_PORT="5432"
@@ -8,10 +7,8 @@ DB_USER="postgres"
 DB_PASSWORD=$DB_PASSWORD
 # SQL script file
 SQL_FILE="./release1.sql"
-
 # Connect to the database
 pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
-
 # Check if authentication was successful
 if [ $? -eq 0 ]; then
     echo "Authentication successful. Executing SQL file..."
