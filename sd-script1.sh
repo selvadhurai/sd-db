a722e0de85bcdc49e30d5b582714bda32c9f5367
Selva <47226341+selvadhurai@users.noreply.github.com>
2024-05-21 10:33:25 -0400

    Update sd-script1.sh
    corrected description

--- sd-script1.sh
+++ sd-script1.sh
@@ -2,20 +2,20 @@
 
 # Database connection parameters
 DB_HOST="sd-db1.c76o4qoeq8xw.us-east-1.rds.amazonaws.com"
-DB_NAME="sd-db1"
-DB_USER="sd-db1"
-DB_PASS="test1234"
+DB_PORT="5432"
+DB_NAME="sd_firstdb"
+DB_USER="postgres"
 # SQL script file
-SQL_FILE="release1.sql"
+SQL_FILE="./release1.sql"
 
 # Connect to the database
-psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
+pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
 
 # Check if authentication was successful
 if [ $? -eq 0 ]; then
     echo "Authentication successful. Executing SQL file..."
     # Execute SQL file
-    psql -h "$DB_HOST" -d "$DB_NAME" -U "$DB_USER" -w -f "$SQL_FILE"
+    psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
     if [ $? -eq 0 ]; then
         echo "SQL file executed successfully"
     else
