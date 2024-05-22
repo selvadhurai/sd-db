04f1be7ae88cd738c7e218711476f624631152eb
Selva <47226341+selvadhurai@users.noreply.github.com>
2024-05-22 12:58:41 -0400

    Update sd-script1.sh

--- sd-script1.sh
+++ sd-script1.sh
@@ -8,17 +8,4 @@ DB_PASSWORD=$DB_PASSWORD
 # SQL script file
 SQL_FILE="./release1.sql"
 # Connect to the database
-pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME"
-# Check if authentication was successful
-if [ $? -eq 0 ]; then
-    echo "Authentication successful. Executing SQL file..."
-    # Execute SQL file
-    psql -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
-    if [ $? -eq 0 ]; then
-        echo "SQL file executed successfully"
-    else
-        echo "Error executing SQL file"
-    fi
-else
-    echo "Authentication failed. Transaction terminated."
-fi
+pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
