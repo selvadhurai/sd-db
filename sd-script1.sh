07647fde5301debe389fd3dfc193a6779e606bb0
Selva <47226341+selvadhurai@users.noreply.github.com>
2024-05-22 13:13:45 -0400

    Update sd-script1.sh

--- sd-script1.sh
+++ sd-script1.sh
@@ -6,6 +6,6 @@ DB_NAME="sd_firstdb"
 DB_USER="postgres"
 DB_PASSWORD=$DB_PASSWORD
 # SQL script file
-SQL_FILE="./release1.sql"
+SQL_FILE=". ./release1.sql"
 # Connect to the database
 pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER" -d "$DB_NAME" -f "$SQL_FILE"
