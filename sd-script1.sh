6f5be0e52c15fd1d2d54ae0e0a9cd58357f0f7f4
Selva <47226341+selvadhurai@users.noreply.github.com>
2024-05-23 13:05:32 -0400

    Update sd-script1.sh

--- sd-script1.sh
+++ sd-script1.sh
@@ -4,7 +4,7 @@ DB_HOST="sd-db1.c76o4qoeq8xw.us-east-1.rds.amazonaws.com"
 DB_PORT="5432"
 DB_NAME="sd_firstdb"
 DB_USER="postgres"
-DB_PASSWORD=$DB_PASSWORD
+DB_PASSWORD="$DB_PASSWORD"
 # SQL script file
 # SQL_FILE=". ./release1.sql"
 export PGPASSWORD="$DB_PASSWORD"
